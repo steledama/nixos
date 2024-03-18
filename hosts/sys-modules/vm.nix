eabc243e632c642c3f18770b86e8642c8cf52ce8
stefano.pompa <stefano.pompa@gmail.com>
2024-03-18 21:22:50 +0100

    readme and network bridge on vm

--- hosts/sys-modules/vm.nix
+++ hosts/sys-modules/vm.nix
@@ -12,5 +12,17 @@
     libvirt
     virt-viewer
   ];
+
+  # networking bridge
+  networking.interfaces.br0 = {
+    ipv4.addresses = [{
+      address = "192.168.1.2";
+      prefixLength = 24;
+    }];
+    bridge.enable = true;
+    bridge.stp = false; # Opzionale, disabilita Spanning Tree Protocol
+    bridge.interfaces = [ "enp7s0" ]; # Sostituisci con il nome della tua interfaccia
+  };
+
 }
 
