9b0737acaf541f1de95105ef4ca1a858c09cfc58
stefano.pompa <stefano.pompa@gmail.com>
2024-03-18 21:31:01 +0100

    vm bridge attempt

--- hosts/sys-modules/vm.nix
+++ hosts/sys-modules/vm.nix
@@ -3,26 +3,16 @@
   # enable virtualization
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+  # dconf
   programs.dconf.enable = true;
   # guest additions
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
   services.spice-webdavd.enable = true;
+  # others
   environment.systemPackages = with pkgs; [
     libvirt
     virt-viewer
   ];
-
-  # networking bridge
-  networking.interfaces.br0 = {
-    ipv4.addresses = [{
-      address = "192.168.1.2";
-      prefixLength = 24;
-    }];
-    bridge.enable = true;
-    bridge.stp = false; # Opzionale, disabilita Spanning Tree Protocol
-    bridge.interfaces = [ "enp7s0" ]; # Sostituisci con il nome della tua interfaccia
-  };
-
 }
 
