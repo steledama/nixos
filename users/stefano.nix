5f6543fb59449d8c1fb190d831fc38a9a1c24de9
stefano.pompa <stefano.pompa@gmail.com>
2024-03-24 09:33:44 +0100

    all termials on user stefano

--- users/stefano.nix
+++ users/stefano.nix
@@ -20,9 +20,9 @@
     # alacritty
     ./usr-modules/alacritty.nix
     # kitty
-    # ./usr-modules/kitty.nix
+    ./usr-modules/kitty.nix
     # wezterm
-    # ./usr-modules/wezterm.nix
+    ./usr-modules/wezterm.nix
     # terminal system info
     ./usr-modules/neofetch.nix
     # bash prompt customization
