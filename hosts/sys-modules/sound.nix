769a421669bfe8c9c67629cd18e1c560cef23094
steledama <stefano.pompa@gmail.com>
2024-07-19 17:56:34 +0200

    flake update and no sound

--- hosts/sys-modules/sound.nix
+++ hosts/sys-modules/sound.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 {
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
