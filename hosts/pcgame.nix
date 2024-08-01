e230b46847e2897ee42afe69b92cffd73270696d
steledama <stefano.pompa@gmail.com>
2024-08-01 21:55:11 +0200

    cleaned hosts config files

--- hosts/pcgame.nix
+++ hosts/pcgame.nix
@@ -95,20 +95,6 @@
     git # version control
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.stefano = {
     isNormalUser = true;
