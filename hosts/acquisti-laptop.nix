f2ee71548722514c0e5401348a0a70d3d5297969
stefano <stefano.pompa@gmail.com>
2024-08-12 18:48:44 +0200

    no python not postgresql

--- hosts/acquisti-laptop.nix
+++ hosts/acquisti-laptop.nix
@@ -48,8 +48,6 @@
       ./sys-modules/touchpad.nix
       # gaming
       # ./sys-modules/gaming.nix
-      # db
-      ./sys-modules/postgre.nix
     ];
 
   # allow unfree software
@@ -67,7 +65,6 @@
     unzip # An extraction utility for archives compressed in .zip format
     usbimager # flash os iso images on usb drive
     gcc # c compiler
-    python3 # python language interpreter
   ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
