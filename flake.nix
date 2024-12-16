8109211a90e998bc824ffbe412953d1c89d752ae
stefano <stefano.pompa@gmail.com>
2024-12-16 13:07:44 +0100

    sviluppo-laptop on flake.nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,45 @@
 {
   description = "Nixos config flake";
 
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     {
       # system configurations
-      nixosConfigurations =
-        {
-          pcgame = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            modules = [
-              ./hosts/pcgame.nix
-            ];
-          };
-          acquisti-laptop = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            modules = [
-              ./hosts/acquisti-laptop.nix
-            ];
-          };
-          # kichstart: (uncomment the following and check the hostname)
-          # nixos = nixpkgs.lib.nixosSystem {
-          #   specialArgs = { inherit inputs; };
-          #   modules = [
-          #     ./hosts/configuration.nix
-          #   ];
-          # };
+      nixosConfigurations = {
+        pcgame = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/pcgame.nix
+          ];
         };
+        acquisti-laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/acquisti-laptop.nix
+          ];
+        };
+        sviluppo-laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/sviluppo-laptop.nix
+          ];
+        };
+        # kichstart: (uncomment the following and check the hostname)
+        # nixos = nixpkgs.lib.nixosSystem {
+        #   specialArgs = { inherit inputs; };
+        #   modules = [
+        #     ./hosts/configuration.nix
+        #   ];
+        # };
+      };
       # HOME-MANAGER as standalone
       # homeConfigurations =
       # {
