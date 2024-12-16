97b6d1ed6971980f3fb02e966530ebd4de619217
stefano <stefano.pompa@gmail.com>
2024-12-16 12:41:11 +0100

    user and home defined

--- users/sviluppo.nix
+++ users/sviluppo.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "stefano";
-  home.homeDirectory = "/home/stefano";
+  home.username = "sviluppo";
+  home.homeDirectory = "/home/sviluppo";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
