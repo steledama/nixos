f125092dd15f548d515e9f3bdf13b16f02eeb773
steledama <stefano.pompa@gmail.com>
2025-01-05 15:55:33 +0100

    secrvice.pulseaudio

--- hosts/sys-modules/sound.nix
+++ hosts/sys-modules/sound.nix
@@ -1,7 +1,7 @@
 {
   # Enable sound with pipewire.
   # sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -16,4 +16,3 @@
     #media-session.enable = true;
   };
 }
-
