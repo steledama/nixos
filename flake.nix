087e7d791e8ba96e7645a566209d78db59b25f6b
steledama <stefano.pompa@gmail.com>
2025-01-10 20:41:54 +0100

    simplified flake

--- flake.nix
+++ flake.nix
@@ -9,31 +9,39 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
 
       # Helper function to create host configurations
-      mkHost = hostname: extraModules: nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/${hostname}/default.nix  # Percorso più specifico
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
-        ] ++ extraModules;
-      };
+      mkHost =
+        hostname: extraModules:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/${hostname}
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+          ] ++ extraModules;
+        };
     in
     {
       # Host configurations
       nixosConfigurations = {
-        pcgame = mkHost "pcgame" [];
-        acquisti-laptop = mkHost "acquisti-laptop" [];
-        sviluppo-laptop = mkHost "sviluppo-laptop" [];
+        pcgame = mkHost "pcgame" [ ];
+        acquisti-laptop = mkHost "acquisti-laptop" [ ];
+        sviluppo-laptop = mkHost "sviluppo-laptop" [ ];
       };
     };
 }
