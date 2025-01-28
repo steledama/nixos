5a2c3c64162f8496c7d6b4cfef961614add37b47
stefano <stefano.pompa@gmail.com>
2025-01-28 20:14:20 +0100

    flake update

--- modules/home/gnome-theme.nix
+++ modules/home/gnome-theme.nix
@@ -3,19 +3,25 @@
 {
   home.packages = with pkgs; [
     # Packages for custom themes
-    catppuccin-gtk
-    catppuccin-cursors
-    papirus-icon-theme
+    # catppuccin-gtk
+    # catppuccin-cursors
+    # papirus-icon-theme
   ];
 
   dconf.settings = {
     "org/gnome/shell/extensions/user-theme" = {
-      name = "Catppuccin-Mocha-Standard-Blue-Dark"; # Default is "Adwaita"
+      # Default is "Adwaita"
+      name = "Adwaita";
+      # name = "Catppuccin-Mocha-Standard-Blue-Dark";
     };
 
     "org/gnome/desktop/interface" = {
-      gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark"; # Default is "Adwaita"
-      icon-theme = "Papirus-Dark"; # Default is "Adwaita"
+      # Default is "Adwaita"
+      gtk-theme = "Adwaita";
+      # gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
+      # Default is "Adwaita"
+      icon-theme = "Adwaita";
+      # icon-theme = "Papirus-Dark";
       cursor-theme = "Adwaita";
     };
   };
