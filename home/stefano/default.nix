94c90947c8143f0a45c40faf9e24d931df049c61
steledama <stefano.pompa@gmail.com>
2025-01-28 20:36:55 +0100

    removed amule from stefano

--- home/stefano/default.nix
+++ home/stefano/default.nix
@@ -16,7 +16,7 @@
 
   # User-specific packages (additional to common ones)
   home.packages = with pkgs; [
-    amule # Peer-to-peer client for the eD2K and Kademlia networks
+    # amule # Peer-to-peer client for the eD2K and Kademlia networks
   ];
 
   # Any user-specific overrides or additional configurations can go here
