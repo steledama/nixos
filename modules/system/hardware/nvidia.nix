7a1fbb7eef2b329f16848578a890754a1991dafa
steledama <stefano.pompa@gmail.com>
2025-02-09 10:20:44 +0100

    better nvidia settings and flake update

--- modules/system/hardware/nvidia.nix
+++ modules/system/hardware/nvidia.nix
@@ -1,23 +1,28 @@
-{ pkgs, config, ... }:
-
+{ config, pkgs, ... }:
 {
-  # Disable nouveau
+  # Disabilita il driver open source "nouveau"
   boot.blacklistedKernelModules = [ "nouveau" ];
 
-  # Enable NVIDIA drivers
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    forceFullCompositionPipeline = true;
+  # Abilita Xorg e imposta il driver Nvidia
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "nvidia" ];
   };
 
-  # Essential NVIDIA packages
+  # Configurazione Nvidia
+  hardware.nvidia = {
+    # Abilita il modesetting (passa nvidia-drm.modeset=1 al kernel)
+    modesetting.enable = true;
+    # Abilita nvidia-settings per verifiche e configurazioni grafiche
+    nvidiaSettings = true;
+    # Utilizza il pacchetto stabile dei driver Nvidia compatibile con il kernel in uso
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # Impostato su false per evitare un carico aggiuntivo; abilitalo se noti tearing
+    forceFullCompositionPipeline = false;
+  };
+
+  # (Opzionale) Pacchetto per il monitoraggio della GPU
   environment.systemPackages = with pkgs; [
-    nvtopPackages.nvidia # Per monitorare la GPU
+    nvtopPackages.nvidia
   ];
 }
