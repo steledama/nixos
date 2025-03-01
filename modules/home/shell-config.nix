b07d68198469d3954207f5995ebacaf542cd401a
steledama <stefano.pompa@gmail.com>
2025-03-01 17:02:49 +0100

    cleaner tmux shortcuts

--- modules/home/shell-config.nix
+++ modules/home/shell-config.nix
@@ -145,9 +145,6 @@
         bind-key -T copy-mode-vi v send-keys -X begin-selection
         bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
 
-        # Custom binding to force save tmux session
-        bind r run-shell "#{resurrect_dir}/save.sh"
-
         # True-color support
         set -g default-terminal "screen-256color"
         set -ga terminal-overrides ",*256col*:Tc"
