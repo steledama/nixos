5a1fe64ba63ed0464fd680ef9d95bf36732935e4
stefano <stefano.pompa@gmail.com>
2025-04-07 10:27:36 +0200

    better comments on msty pkg

--- pkgs/msty.nix
+++ pkgs/msty.nix
@@ -2,36 +2,36 @@
 #
 # Msty AppImage packaging for NixOS
 #
-# How to update this package:
-# 1. Check if a new version is available (current: 1.8.1)
-# 2. Calculate the new hash using one of these methods:
-#    - For SRI format: nix-prefetch-url --type sha256 https://assets.msty.app/prod/latest/linux/amd64/Msty_x86_64_amd64.AppImage
-#    - OR convert base32 to SRI format: nix hash convert --to-sri --type sha256 THE_BASE32_HASH
-# 3. Update the version number and hash below
-# 4. Rebuild your system: sudo nixos-rebuild switch --flake .
-{
-  appimageTools,
-  fetchurl,
-  makeWrapper,
-}: let
+# To update this package check if a new version is available and calculate the new hash:
+# nix hash convert --hash-algo sha256 --to sri --from nix32 $(nix-prefetch-url --type sha256 https://assets.msty.app/prod/latest/linux/amd64/Msty_x86_64_amd64.AppImage)
+# Update the version number and hash below
+# Rebuild your system:
+#    sudo nixos-rebuild switch --flake .
+
+{ appimageTools
+, fetchurl
+, makeWrapper
+,
+}:
+let
   pname = "msty";
   version = "1.8.4"; # Update this when a new version is released
   src = fetchurl {
     url = "https://assets.msty.app/prod/latest/linux/amd64/Msty_x86_64_amd64.AppImage";
     hash = "sha256-4NjS9/ZlzFWyVHA054DmpHeTl35PgkPiHwgRjHeB4is=";
   };
-  appimageContents = appimageTools.extractType2 {inherit pname version src;};
+  appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
-  appimageTools.wrapType2 {
-    inherit pname version src;
-    nativeBuildInputs = [makeWrapper];
-    extraInstallCommands = ''
-      install -m 444 -D ${appimageContents}/msty.desktop -t $out/share/applications
-      substituteInPlace $out/share/applications/msty.desktop \
-              --replace 'Exec=AppRun' 'Exec=${pname}'
-      install -m 444 -D ${appimageContents}/msty.png \
-              $out/share/icons/hicolor/256x256/apps/msty.png
-      wrapProgram $out/bin/${pname} \
-              --set XDG_CURRENT_DESKTOP GNOME
-    '';
-  }
+appimageTools.wrapType2 {
+  inherit pname version src;
+  nativeBuildInputs = [ makeWrapper ];
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/msty.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/msty.desktop \
+            --replace 'Exec=AppRun' 'Exec=${pname}'
+    install -m 444 -D ${appimageContents}/msty.png \
+            $out/share/icons/hicolor/256x256/apps/msty.png
+    wrapProgram $out/bin/${pname} \
+            --set XDG_CURRENT_DESKTOP GNOME
+  '';
+}
