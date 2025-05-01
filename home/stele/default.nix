b6f5c8a8ce14d3e8bf596fe9de9dc0e7f4d27e68
steledama <stefano.pompa@gmil.com>
2025-05-01 19:41:37 +0200

    background on minibook

--- home/stele/default.nix
+++ home/stele/default.nix
@@ -17,6 +17,13 @@
   # Setup WMs (default us keyboard)
   wayland-wm = {
     enable = true;
+    # Wallpaper (To find the correct output names for each system, you should run wlr-randr or niri msg outputs when running Niri to see the available outputs.)
+    wallpaper = {
+      outputColors = {
+        "DS-1" = "#000000";
+        # "DP-2" = "#660033";
+      };
+    };
   };
 
   # User-specific packages (additional to common ones)
