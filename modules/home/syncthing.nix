3c1261a9da1a983eaf4e39ccddded0634b7e267d
sviluppo <sviluppo@toscanatradingsrl.com>
2025-07-15 09:34:25 +0200

    bug fix on syncthing module

--- modules/home/syncthing.nix
+++ modules/home/syncthing.nix
@@ -3,15 +3,7 @@
   services = {
     syncthing = {
       enable = true;
-      # tray.enable = true; # tray
-      # user = "acquisti"; # user
-      # dataDir = "/home/acquisti"; # Default folder for new synced folders
-      # configDir = "/home/acquisti/.config/syncthing"; # Folder for Syncthing's settings and keys
-      settings = {
-        gui = {
-          address = "0.0.0.0:8384";
-        };
-      };
+      guiAddress = "0.0.0.0:8384";
     };
   };
 }
