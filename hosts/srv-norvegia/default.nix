70103162b79d5946e6bd552265b77e48c753baa3
sviluppo <sviluppo@toscanatradingsrl.com>
2025-07-17 10:15:01 +0200

    firewall test per server

--- hosts/srv-norvegia/default.nix
+++ hosts/srv-norvegia/default.nix
@@ -26,7 +26,7 @@
     hostName = "srv-norvegia";
     networkmanager.enable = true;
     firewall = {
-      enable = true;
+      enable = false;
       allowedTCPPorts = [
         22 # ssh
         80 # nginx
