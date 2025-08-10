1bcf9c372a59da4188b6039fb2dee16c2eb73760
steledama <stefano.pompa@gmil.com>
2025-08-10 16:30:57 +0200

    gemini-cli

--- modules/home/dev-tools.nix
+++ modules/home/dev-tools.nix
@@ -9,26 +9,27 @@
     direnv # Shell extension that manages your environment
     openssl # Cryptographic library that implements the SSL and TLS protocols
     claude-code # Agentic coding tool
-    
+    gemini-cli # AI agent into your terminal
+
     # Language runtimes
     nodejs # Event-driven I/O framework for the V8 JavaScript engine
     rustup # Rust toolchain installer (includes cargo)
     python3 # High-level dynamically-typed programming language
-    
+
     # Language servers
     nil # Nix
     nixd # Nix
     lua-language-server # Lua
     typescript-language-server # Js
     pyright # python
-    
+
     # Formatters
     prettierd # JavaScript
     stylua # Lua
     alejandra # Nix
     black # Python
     isort # Python
-    
+
     # Linters
     eslint # JavaScript
     lua54Packages.luacheck # Lua
