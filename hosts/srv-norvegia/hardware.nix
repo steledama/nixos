28ee08faddf369fc7d350891e6033255c9b1ab98
sviluppo <sviluppo@toscanatradingsrl.com>
2025-09-24 11:37:05 +0200

    new hardware partition for srv-norvegia

--- hosts/srv-norvegia/hardware.nix
+++ hosts/srv-norvegia/hardware.nix
@@ -5,8 +5,7 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
@@ -15,20 +14,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/05bd0ffc-646e-4cfb-9523-afbd5b94e1dc";
+    { device = "/dev/disk/by-uuid/1b4990c8-a846-4fe2-8c3a-55c3a8c3c110";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/DAD1-E3E4";
+    { device = "/dev/disk/by-uuid/4C02-0203";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/a7c001bd-092b-4b3f-a234-e29a03977e61"; }];
+    [ { device = "/dev/disk/by-uuid/a47eb4f8-41cc-49c6-b86f-993312e67e7d"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
